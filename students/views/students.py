4bcda2cb96011ad8d5f4730b4b2830fd371f8e18
Oleksii Borys <bo-a@yandex.ru>
2016-10-26 06:55:36 -0700

    p254

--- students/views/students.py
+++ students/views/students.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import Http404
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -11,34 +11,27 @@ from ..models import Student
 
 
 def students_list(request):
-    # raise Http404
-    # return HttpResponse('<h1>Hello World</h1>')
-    # students = (
-    #     {'id': 1,
-    #      'first_name': u'John',
-    #      'last_name': u'Connor',
-    #      'ticket': 333,
-    #      'image': 'img/default.jpg'
-    #      },
-    #     {'id': 2,
-    #      'first_name': u'Arnold',
-    #      'last_name': u'Shwarcenegger',
-    #      'ticket': 555,
-    #      'image': 'img/default.jpg'
-    #      },
-    #     {'id': 3,
-    #      'first_name': u'Александр',
-    #      'last_name': u'Орєхов',
-    #      'ticket': 777,
-    #      'image': 'img/default.jpg'
-    #      },
-    # )
     students = Student.objects.all()
+
+    # try to order students list
     order_by = request.GET.get('order_by', '')
     if order_by in ('last_name', 'first_name', 'ticket'):
         students = students.order_by(order_by)
         if request.GET.get('reverse', '') == '1':
             students = students.reverse()
+
+    # paginate students
+    paginator = Paginator(students, 3)
+    page = request.GET.get('page')
+    try:
+        students = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page
+        students = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999),
+        # deliver last page of results.
+        students = paginator.page(paginator.num_pages)
 
     return render(request, 'students/students_list.html', {'students': students})
 
@@ -53,4 +46,3 @@ def students_edit(request, sid):
 
 def students_delete(request, sid):
     return HttpResponse('<h1>Delete Student %s</h1>' % sid)
-
